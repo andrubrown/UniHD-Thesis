d768df07f7c7b4c7156de9132b7444f0d528bca7
Kushal Khandelwal <kushal124@gmail.com>
2015-05-04 21:38:16 +0200

    update

---
+++ phd-thesis-template/Chapter2/codes/clusterexample.py
@@ -0,0 +1,46 @@
+from sklearn import metrics
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+# Generate sample data
+centers = [[1, 1], [-1, -1], [1, -1]]
+X, labels_true = make_blobs(n_samples=750, centers=centers, cluster_std=0.4,
+                            random_state=0)
+
+X = StandardScaler().fit_transform(X)
+
+# Find clusters
+kmeans = KMeans(init='k-means++', n_clusters=3, n_init=10)
+kmeans.fit(X)
+
+h = .02 
+# Plot the decision boundary. For that, we will assign a color to each
+x_min, x_max = X[:, 0].min() + 1, X[:, 0].max() - 1
+y_min, y_max = X[:, 1].min() + 1, X[:, 1].max() - 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+# Obtain labels for each point in mesh. Use last trained model.
+Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.figure(1)
+plt.clf()
+plt.imshow(Z, interpolation='nearest',
+           extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+           cmap=plt.cm.Paired,
+           aspect='auto', origin='lower')
+
+plt.plot(X[:, 0], X[:, 1], 'k.', markersize=2)
+# Plot the centroids as a white X
+centroids = kmeans.cluster_centers_
+plt.scatter(centroids[:, 0], centroids[:, 1],
+            marker='x', s=169, linewidths=3,
+            color='w', zorder=10)
+plt.title('K-means clustering on the digits dataset (PCA-reduced data)\n'
+          'Centroids are marked with white cross')
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xticks(())
+plt.yticks(())
+plt.show()
